0c87dce5134190e48675ccb4ca9c360ecdff0b5d
Minxing Luo <mxluocv@gmail.com>
2025-10-24 17:57:37 +0800

    Changing to TIGER

--- src/App.vue
+++ src/App.vue
@@ -67,23 +67,27 @@ onMounted(() => {
   <h1 style="text-align: center; font-size: 30px; margin: 15px 0px 5px 0px;"><strong>Time-Aware One Step Diffusion Network for Real-World Image Super-Resolution</strong></h1>
 
   <p id="authors" style="text-align: center; margin: 10px 0px;">
-    <a href="" style="font-size: 20px;" >Tainyi Zhang<sup>1</sup></a>, 
-    <a href="https://adam-duan.github.io/" style="font-size: 20px;" >Zheng-Peng Duan<sup>1</sup></a>, 
-    <a href="" style="font-size: 20px;" >Peng-Tao Jiang<sup>2</sup></a>, 
-    <a href="" style="font-size: 20px;" >Bo Li<sup>2</sup></a>,<br>
-    <a href="" style="font-size: 20px;" >MingMing Cheng<sup>1</sup></a>,
-    <a href="https://scholar.google.com/citations?user=RZLYwR0AAAAJ&hl=zh-CN&oi=ao" style="font-size: 20px;" >Chun-Le Guo<sup>1,3,&dagger;</sup></a>,
-    <a href="https://li-chongyi.github.io/" style="font-size: 20px;">Chongyi Li<sup>1,3</sup></a>
+    <a href="" style="font-size: 20px;" >Minxing Luo<sup>1,2,*</sup></a>, 
+    <a href="" style="font-size: 20px;" >Linlong Fan<sup>2,*</sup></a>, 
+    <a href="" style="font-size: 20px;" >Wang Qiushi<sup>2,3</sup></a>, 
+    <a href="" style="font-size: 20px;" >Ge Wu<sup>1</sup></a>,<br>
+    <a href="" style="font-size: 20px;" >Yiyan Luo<sup>2</sup></a>,
+    <a href="" style="font-size: 20px;" >Jinwei Chen<sup>2</sup></a>,
+    <a href="https://yaxingwang.github.io/" style="font-size: 20px;" >Yaxing Wang<sup>1</sup></a>,
+    <a href="https://fqnchina.github.io/" style="font-size: 20px;" >Qingnan Fan<sup>2,&dagger;</sup></a>,
+    <a href="" style="font-size: 20px;">Jian Yang<sup>1,&dagger;</sup></a>
     <br>
     <span style="font-size: 18px; font-weight: bold; ;"><sup>1</sup>VCIP, CS, Nankai University. </span> &nbsp;&nbsp;
-    <span style="font-size: 18px; font-weight: bold; ;"><sup>2</sup>vivo Mobile Communication Co. Ltd.</span> <br>
-    <span style="font-size: 18px; font-weight: bold; ;"><sup>3</sup>NKIARI, Shenzhen Futian.</span> &nbsp;&nbsp;
+    <span style="font-size: 18px; font-weight: bold; ;"><sup>2</sup>vivo Mobile Communication Co. Ltd.</span> 
+    <span style="font-size: 18px; font-weight: bold; ;"><sup>*;</sup>Corresponding author.</span>
+    <br>
+    <span style="font-size: 18px; font-weight: bold; ;"><sup>3</sup>SDS, The Chinese University of Hong Kong, Shenzhen.</span> &nbsp;&nbsp;
     <span style="font-size: 18px; font-weight: bold; ;"><sup>&dagger;</sup>Corresponding author.</span> <br>
 
   </p>
 
   <div class="button-container">
-    <a href="https://arxiv.org/abs/2508.16557" target="_blank" class="custom-button">
+    <a href="https://arxiv.org/" target="_blank" class="custom-button">
       <svg class="arxiv-icon" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg" style="margin-right: 5px;"> 
         <path
          fill="currentColor"
@@ -94,21 +98,13 @@ onMounted(() => {
       <span>arXiv</span>
     </a>
     &nbsp;&nbsp;
-    <a href="https://github.com/zty557/TADSR" target="_blank" class="custom-button">
+    <a href="https://github.com/Tony-Lowe/TIGER_project_page" target="_blank" class="custom-button">
       <span class="icon" style="margin-right: 10px;">
         <i class="fab fa-github"></i>
       </span>
-      <span>Code</span>
+      <span>Code (Coming Soon)</span>
     </a>
     &nbsp;&nbsp;
-    <a href="" target="_blank" class="custom-button">
-      <svg fill="currentColor" fill-rule="evenodd" height="1em" style="flex:none;line-height:1;margin-right: 3px;" viewBox="0 0 24 24" width="1em" xmlns="http://www.w3.org/2000/svg">
-        <title>HuggingFace</title>
-        <path d="M16.781 3.277c2.997 1.704 4.844 4.851 4.844 8.258 0 .995-.155 1.955-.443 2.857a1.332 1.332 0 011.125.4 1.41 1.41 0 01.2 1.723c.204.165.352.385.428.632l.017.062c.06.222.12.69-.2 1.166.244.37.279.836.093 1.236-.255.57-.893 1.018-2.128 1.5l-.202.078-.131.048c-.478.173-.89.295-1.061.345l-.086.024c-.89.243-1.808.375-2.732.394-1.32 0-2.3-.36-2.923-1.067a9.852 9.852 0 01-3.18.018C9.778 21.647 8.802 22 7.494 22a11.249 11.249 0 01-2.541-.343l-.221-.06-.273-.08a16.574 16.574 0 01-1.175-.405c-1.237-.483-1.875-.93-2.13-1.501-.186-.4-.151-.867.093-1.236a1.42 1.42 0 01-.2-1.166c.069-.273.226-.516.447-.694a1.41 1.41 0 01.2-1.722c.233-.248.557-.391.917-.407l.078-.001a9.385 9.385 0 01-.44-2.85c0-3.407 1.847-6.554 4.844-8.258a9.822 9.822 0 019.687 0zM4.188 14.758c.125.687 2.357 2.35 2.14 2.707-.19.315-.796-.239-.948-.386l-.041-.04-.168-.147c-.561-.479-2.304-1.9-2.74-1.432-.43.46.119.859 1.055 1.42l.784.467.136.083c1.045.643 1.12.84.95 1.113-.188.295-3.07-2.1-3.34-1.083-.27 1.011 2.942 1.304 2.744 2.006-.2.7-2.265-1.324-2.685-.537-.425.79 2.913 1.718 2.94 1.725l.16.04.175.042c1.227.284 3.565.65 4.435-.604.673-.973.64-1.709-.248-2.61l-.057-.057c-.945-.928-1.495-2.288-1.495-2.288l-.017-.058-.025-.072c-.082-.22-.284-.639-.63-.584-.46.073-.798 1.21.12 1.933l.05.038c.977.721-.195 1.21-.573.534l-.058-.104-.143-.25c-.463-.799-1.282-2.111-1.739-2.397-.532-.332-.907-.148-.782.541zm14.842-.541c-.533.335-1.563 2.074-1.94 2.751a.613.613 0 01-.687.302.436.436 0 01-.176-.098.303.303 0 01-.049-.06l-.014-.028-.008-.02-.007-.019-.003-.013-.003-.017a.289.289 0 01-.004-.048c0-.12.071-.266.25-.427.026-.024.054-.047.084-.07l.047-.036c.022-.016.043-.032.063-.049.883-.71.573-1.81.131-1.917l-.031-.006-.056-.004a.368.368 0 00-.062.006l-.028.005-.042.014-.039.017-.028.015-.028.019-.036.027-.023.02c-.173.158-.273.428-.31.542l-.016.054s-.53 1.309-1.439 2.234l-.054.054c-.365.358-.596.69-.702 1.018-.143.437-.066.868.21 1.353.055.097.117.195.187.296.882 1.275 3.282.876 4.494.59l.286-.07.25-.074c.276-.084.736-.233 1.2-.42l.188-.077.065-.028.064-.028.124-.056.081-.038c.529-.252.964-.543.994-.827l.001-.036a.299.299 0 00-.037-.139c-.094-.176-.271-.212-.491-.168l-.045.01c-.044.01-.09.024-.136.04l-.097.035-.054.022c-.559.23-1.238.705-1.607.745h.006a.452.452 0 01-.05.003h-.024l-.024-.003-.023-.005c-.068-.016-.116-.06-.14-.142a.22.22 0 01-.005-.1c.062-.345.958-.595 1.713-.91l.066-.028c.528-.224.97-.483.985-.832v-.04a.47.47 0 00-.016-.098c-.048-.18-.175-.251-.36-.251-.785 0-2.55 1.36-2.92 1.36-.025 0-.048-.007-.058-.024a.6.6 0 01-.046-.088c-.1-.238.068-.462 1.06-1.066l.209-.126c.538-.32 1.01-.588 1.341-.831.29-.212.475-.406.503-.6l.003-.028c.008-.113-.038-.227-.147-.344a.266.266 0 00-.07-.054l-.034-.015-.013-.005a.403.403 0 00-.13-.02c-.162 0-.369.07-.595.18-.637.313-1.431.952-1.826 1.285l-.249.215-.033.033c-.08.078-.288.27-.493.386l-.071.037-.041.019a.535.535 0 01-.122.036h.005a.346.346 0 01-.031.003l.01-.001-.013.001c-.079.005-.145-.021-.19-.095a.113.113 0 01-.014-.065c.027-.465 2.034-1.991 2.152-2.642l.009-.048c.1-.65-.271-.817-.791-.493zM11.938 2.984c-4.798 0-8.688 3.829-8.688 8.55 0 .692.083 1.364.24 2.008l.008-.009c.252-.298.612-.46 1.017-.46.355.008.699.117.993.312.22.14.465.384.715.694.261-.372.69-.598 1.15-.605.852 0 1.367.728 1.562 1.383l.047.105.06.127c.192.396.595 1.139 1.143 1.68 1.06 1.04 1.324 2.115.8 3.266a8.865 8.865 0 002.024-.014c-.505-1.12-.26-2.17.74-3.186l.066-.066c.695-.684 1.157-1.69 1.252-1.912.195-.655.708-1.383 1.56-1.383.46.007.889.233 1.15.605.25-.31.495-.553.718-.694a1.87 1.87 0 01.99-.312c.357 0 .682.126.925.36.14-.61.215-1.245.215-1.898 0-4.722-3.89-8.55-8.687-8.55zm1.857 8.926l.439-.212c.553-.264.89-.383.89.152 0 1.093-.771 3.208-3.155 3.262h-.184c-2.325-.052-3.116-2.06-3.156-3.175l-.001-.087c0-1.107 1.452.586 3.25.586.716 0 1.379-.272 1.917-.526zm4.017-3.143c.45 0 .813.358.813.8 0 .441-.364.8-.813.8a.806.806 0 01-.812-.8c0-.442.364-.8.812-.8zm-11.624 0c.448 0 .812.358.812.8 0 .441-.364.8-.812.8a.806.806 0 01-.813-.8c0-.442.364-.8.813-.8zm7.79-.841c.32-.384.846-.54 1.33-.394.483.146.83.564.878 1.06.048.495-.212.97-.659 1.203-.322.168-.447-.477-.767-.585l.002-.003c-.287-.098-.772.362-.925.079a1.215 1.215 0 01.14-1.36zm-4.323 0c.322.384.377.92.14 1.36-.152.283-.64-.177-.925-.079l.003.003c-.108.036-.194.134-.273.24l-.118.165c-.11.15-.22.262-.377.18a1.226 1.226 0 01-.658-1.204c.048-.495.395-.913.878-1.059a1.262 1.262 0 011.33.394z"></path>
-      </svg>
-      &nbsp;&nbsp;
-      <span>Model</span>
-    </a>
   </div>
 
 
@@ -144,15 +140,7 @@ onMounted(() => {
 
   <div class="content-abstract">
     <h2 style="text-align:center;">Abstract</h2>
-    <p>Diffusion-based real-world image super-resolution (Real-ISR) methods have demonstrated impressive performance. 
-      To achieve efficient Real-ISR, many works employ Variational Score Distillation (VSD) to distill pre-trained stable-diffusion (SD) model for one-step SR with a fixed timestep. 
-      However, due to the different noise injection timesteps, the SD will perform different generative priors. 
-      Therefore, a fixed timestep is difficult for these methods to fully leverage the generative priors in SD, leading to suboptimal performance. 
-      To address this, we propose a Time-Aware one-step Diffusion Network for Real-ISR (<strong>TADSR</strong>). 
-      We first introduce a Time-Aware VAE Encoder, which projects the same image into different latent features based on timesteps. 
-      Through joint dynamic variation of timesteps and latent features, the student model can better align with the input pattern distribution of the pre-trained SD, thereby enabling more effective utilization of SD's generative capabilities. 
-      To better activate the generative prior of SD at different timesteps, we propose a Time-Aware VSD loss that bridges the timesteps of the student model and those of the teacher model, thereby producing more consistent generative prior guidance conditioned on timesteps. 
-      Additionally, though utilizing the generative prior in SD at different timesteps, our method can naturally achieve controllable trade-offs between fidelity and realism by changing the timestep condition. Experimental results demonstrate that our method achieves both state-of-the-art performance and controllable SR results with only a single step. </p>
+    <p>Current generative super-resolution methods show strong performance on natural images but distort text, creating a fundamental trade-off between image quality and textual readability. To address this, we introduce <strong>TIGER</strong> (<strong>T</strong>ext–<strong>I</strong>mage <strong>G</strong>uided sup<strong>E</strong>r-<strong>R</strong>esolution), a novel two-stage framework that breaks this trade-off through a <em>"text-first, image-later"</em> paradigm. TIGER explicitly decouples glyph restoration from image enhancement: it first reconstructs precise text structures and then uses them to guide subsequent full-image super-resolution. This glyph-to-image guidance ensures both high fidelity and visual consistency. To support comprehensive training and evaluation, we also contribute the <strong>UltraZoom-ST</strong> (UltraZoom-Scene Text), the first scene text dataset with extreme zoom (<strong>×14.29</strong>). Extensive experiments show that TIGER achieves <strong>state-of-the-art</strong> performance, enhancing readability while preserving overall image quality. </p>
   </div>
   <!-- <div class="content">
     <h2>Background</h2>
@@ -162,17 +150,43 @@ onMounted(() => {
   </div> -->
   <div class="content">
     <h2 style="text-align:center;">Method</h2>
-    <p>TADSR is a single step real-word image super-resolution method based on diffusion model. 
-      Gigen a low-quality inputs, TADSR is capable of reconstructing high-quality images at various generation levels, conditioned on the input timestep \(t_{s}\). </p>
+    <p>Current image super-resolution methods focus on enhancing overall image quality but often fail to accurately preserve glyph structures, leading to distorted text in super-resolved images. 
+    Conversely, text image super-resolution methods improve text readability but do not retain the global semantic information of the background, causing incoherence between text and non-text regions.
+   </p>
     <br>
     <figure>
-      <img src="/TADSR_files/overview.png" alt="Overview image" style="width: 100%;">
+      <img src="/TIGER_files/overview.png" alt="Overview image" style="width: 100%;">
     </figure>
     <p>
-      TADSR consists of two components: Time-Aware VAE Encoder (TAE) and Time-Aware Variational Score Distillation (TAVSD).
-      TAE projects the low-quality input images into different latent distributions conditioned on timestep \(t_{s}\), in order to better leverage the generative priors of the pre-trained Stable Diffusion (SD) model.
-      TAVSD associates the timestep of the student model and the teacher model, enabling the teacher to provide more consistent gradient supervision conditioned on the timestep \(t_{s}\).
-      By fully exploiting the diverse generative priors of SD across different time steps, TADSR naturally achieves a controllable trade-off between fidelity and generation.  
+  To leverage the strengths of both approaches, we introduce the TIGER framework.
+  As illustrated in the pipeline figure, the TIGER framework is composed of two stages: the <strong>Text Restoration stage</strong> and the <strong>Image Enhancement stage</strong>.
+  In the text restoration stage, we extract the text regions from the LR input \(x_{L} \in \mathbb{R}^{H \times W \times C}\) and feed them into the glyph structure restoration model to restore the text structure based on the text region of the LR input and the predicted text.
+  We then reassemble the text structures to their original positions to obtain a text mask \(\hat{x}_{m} \in \mathbb{R}^{H \times W \times C}\).
+  In the image enhancement stage, the text mask and LR input are then processed by a ControlNet-like network to obtain the enhanced SR output \(\hat{x}_{H} \in \mathbb{R}^{H \times W \times C}\).
+    </p>
+  </div>
+
+  <div class="content">
+    <h2 style="text-align:center;">Dataset and Benchmarks</h2>
+    <p>Existing datasets offer only subtle degradation, making them insufficient for evaluating model robustness.
+    To address the lack of both challenging and publicly available datasets tailored for scene text image super-resolution, especially for Chinese text, we introduce <strong>UltraZoom-ST</strong>, a challenging real-world benchmark.
+   </p>
+    <br>
+    <figure>
+      <img src="/TIGER_files/datapipe.png" alt="Overview of UltraZoom-ST. (a) Real-CE LRs show only mild degradation (red box), while UltraZoom-ST LRs exhibit stronger degradation (red box), enabling a more comprehensive evaluation. (b) Coarse-to-fine alignment: images are sorted by focal length, each warped to the next higher-focal neighbor using an estimated homography matrix, then refined to the 200 mm ground truth." style="width: 100%;">
+    </figure>
+    <figure>
+      <img src="/TIGER_files/datasamples.png" alt="Overview image" style="width: 100%;">
+    </figure>
+    <p>
+    It is collected using ViVO X200 Ultra equipped with four fixed focal lengths (14 mm, 35 mm, 85 mm, 200 mm), enabling image pairs with extreme ×14.29 zoom.
+    However, such extreme zoom introduces severe misalignment that breaks registration methods, originally designed for moderate ×2 to ×4 zoom.
+    To overcome this, we design a <strong>Cascade Coarse-to-fine Alignment</strong> pipeline: images are first sorted by focal length, and each low-quality image is sequentially aligned to its next higher-focal neighbor using either optimization-based registration or feature-based extractors (e.g., SIFT, SURF, RIPE), then refined against the 200 mm ground truth for precise alignment.
+    Through meticulous annotating and aligning, we obtained a total of 5,036 image pairs, with 49,675 lines of text.
+    We set images under 200 mm focal length as ground truth, and obtain 1,439, 1,798, and 1,799 pairs for ×14.29, ×5.71, and ×2.35 zooming modes, respectively.
+    Among them, we randomly select 470, 589, and 581 pairs for evaluation under each zooming mode.
+    Each image pair contains one or more text lines.
+    These evaluation sets enable us to evaluate the performance of models under more complex and challenging scenarios.
     </p>
   </div>
 
@@ -180,11 +194,13 @@ onMounted(() => {
     <h2 style="text-align:center;">Results</h2>
     <h2>Quantitative Comparisons</h2>
     <figure>
-      <img src="/TADSR_files/Quantitative_Comparisons.png" alt="Quantitative_Comparisons" style="width: 100%;">
+      <img src="/TIGER_files/Quantitative_Comparisons.png" alt="Quantitative_Comparisons" style="width: 100%;">
+    </figure>
+    <figure>
+      <img src="/TIGER_files/Quantitative_Comparisons_1.png" alt="Quantitative_Comparisons on Cropped Images" style="width: 100%;">
     </figure>
     <p>
-      TADSR achieves the best performance in almost all the no-reference metircs like MAINIQA, CLIPIQA, TOPIQ, and QALIGN, while also exhibiting competitive reference metrics like PSNR. 
-      This demonstrates that TADSR leverages the generative priors of the pre-trained SD more effectively, resulting in superior image reconstruction quality.
+      TIGER outperforms competing methods on both the Real-CE and UltraZoom-ST benchmarks in terms of image quality and text accuracy, owing to the decoupling strategy.
     </p>
     <h2>Visual Comparisons</h2>
     <figure>
